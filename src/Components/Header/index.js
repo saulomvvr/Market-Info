d87f941d26397ff98943f91be76d3caa63ea9e1a
jfmartinsvred1 <jfmartinsvred@gmail.com>
2024-04-20 20:53:14 -0300

    order coins

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,16 +4,35 @@ import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
 
 
-const Header = ({ changeTheme, themeStatus, themeNowHF, themeNow }) => {
+const Header = ({ changeTheme, themeStatus, themeNowHF, themeNow,updateOrder }) => {
+
+    const textHeader = "font-weight-bold text-capitalize"
 
     return (
         <Fragment>
-            <div className="m-0 p-2 mw-100 w-auto mh-10 h-10 d-flex justify-content-between align-middle" style={{ backgroundColor: themeNowHF }}>
-                <Link to={"/"} style={{textDecoration:"none"}}>
-                    <h1 className="font-weight-bold text-capitalize" style={{ color: themeNow }}>
-                        market info
-                    </h1>
-                </Link>
+            <div className="m-0 p-4 mw-100 w-auto mh-10 h-10 d-flex justify-content-between align-middle" style={{ backgroundColor: themeNowHF }}>
+                <div className='d-flex align-items-baseline gap-4'>
+                    <Link to={"/"} style={{ textDecoration: "none" }}>
+                        <h1 className={textHeader} style={{ color: themeNow }} >
+                            market info
+                        </h1>
+                    </Link>
+                    <Link to={"/"} style={{ textDecoration: "none" }} onClick={(e)=>{updateOrder("Popular")}}>
+                        <h3 className={textHeader} style={{ color: themeNow }}>
+                            Popular
+                        </h3>
+                    </Link>
+                    <Link to={"/"} style={{ textDecoration: "none" }} onClick={(e)=>{updateOrder("EmAlta")}}>
+                        <h3 className={textHeader} style={{ color: themeNow }}>
+                            Em Alta
+                        </h3>
+                    </Link>
+                    <Link to={"/"} style={{ textDecoration: "none" }} onClick={(e)=>{updateOrder("EmBaixa")}}>
+                        <h3 className={textHeader} style={{ color: themeNow }}>
+                            Em Baixa
+                        </h3>
+                    </Link>
+                </div>
                 <Form className='d-flex align-self-center '>
                     <Form.Check
                         type="switch"

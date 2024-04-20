b4f1cea418c4b09eacec79d340829426728cf7bd
saulomv <54077670+saulomvvr@users.noreply.github.com>
2024-04-19 23:57:12 -0300

    Header and foot theme finished

--- src/App.js
+++ src/App.js
@@ -3,19 +3,41 @@ import './App.css';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Home from './Components/Home';
+import { useState } from 'react';
 
 function App() {
+  const lightColor = "#FFFFFF";
+  const darkColor = "#000021";
+  const [mode, setMode] = useState("Light");
+  const [themeStatus, setThemeStatus] = useState("☀️");
+  const [themeNow, setThemeNow] = useState(lightColor);
+  const [themeNowHF, setThemeNowHF] = useState(darkColor);
+
+  function changeTheme(){
+      if(mode === "Light"){
+        setMode("Dark");
+        setThemeStatus("🌑");
+        setThemeNow(darkColor);
+        setThemeNowHF(lightColor);
+      }
+      else{
+        setMode("Light");
+        setThemeStatus("☀️");
+        setThemeNow(lightColor);
+        setThemeNowHF(darkColor);
+      }
+    };
   return (
     <>
-      <Header />
-      <div style={{height:"1000px"}}>
+      <Header changeTheme = { changeTheme } themeStatus = { themeStatus } themeNowHF = { themeNowHF } themeNow = { themeNow } />
+      <div style={{height:"1000px", backgroundColor: themeNow}}>
         <BrowserRouter>
           <Routes>
             <Route path='/' element={<Home></Home>}/>
           </Routes>
         </BrowserRouter>
       </div>
-      <Footer />
+      <Footer changeTheme = { changeTheme } themeStatus = { themeStatus } themeNowHF = { themeNowHF }/>
     </>
   );
 }

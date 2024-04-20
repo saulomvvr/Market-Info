b005a62ce9c4eda210e895db559f6754b3834705
jfmartinsvred1 <jfmartinsvred@gmail.com>
2024-04-19 23:42:15 -0300

    Add icons of coins

--- src/Components/Coin/index.js
+++ src/Components/Coin/index.js
@@ -1,9 +1,12 @@
+import btc from '../../Imgs/icons_coins/bitcoin.png'
 const Coin = ({coin}) =>{
     return(
-        <div className="d-flex flex-column align-items-base p-3 rounded" style={{backgroundColor:"#603c99"}}>
-            <h3 className="text-dark">{coin.code}</h3>
-            <h3 className="text-dark">{coin.name}</h3>
-            <h3 className="text-dark">{coin.value}</h3>
+        <div className="d-flex flex-column align-items-base shadow p-3 mb-5 rounded" style={{backgroundColor:"#603c99", width:"280px"}}>
+            <div className='d-flex align-items-center'>
+                <img src={coin.img} width={"32px"} height={"32px"} className='mx-2'></img>
+                <h3 className="text-light">{`${coin.code}`}</h3>
+            </div>
+            <h3 className="text-light px-2">{`USD : ${coin.value.toFixed(2)}`}</h3>
         </div>
     )
 }
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,21 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Coin from '../Coin';
+import bitcoin from '../../Imgs/icons_coins/bitcoin.png'
+import solana from '../../Imgs/icons_coins/solana.png'
+import ethereum from '../../Imgs/icons_coins/ethereum.png'
 const Home = () =>{
     const cotations = [
         {
             name:"Bitcoin",
             value:63600,
-            code:"BTC"
+            code:"BTC",
+            img:bitcoin
         },
         {
             name:"Ethereum",
             value:3046,
-            code:"ETH"
+            code:"ETH",
+            img:ethereum
         },
         {
             name:"Solana",
             value:141,
-            code:"SOL"
+            code:"SOL",
+            img:solana
         },
     ]
 
@@ -23,7 +29,7 @@ const Home = () =>{
         <section className=''>
             <div className='m-5 d-flex justify-content-around'>
             {cotations.map((coin,index)=>{
-                return <Coin coin={coin} key={index}/>
+                return <Coin coin={coin} key={index} />
             })}
             </div>
         </section>

6125562bb0f4d6d61f8b5a8d0a7c19c7ff6d6f41
jfmartinsvred1 <jfmartinsvred@gmail.com>
2024-04-20 21:08:28 -0300

    Variant of coins complete

--- src/Components/Coin/index.js
+++ src/Components/Coin/index.js
@@ -4,14 +4,18 @@ import btc from '../../Imgs/icons_coins/bitcoin.png';
 const Coin = ({coin, receiveCoin}) =>{
     return(
         <Link to={"/information"} onClick={(e)=>{receiveCoin(coin)}} style={{textDecoration:"none"}}>
-            <div className="d-flex flex-column align-items-base shadow p-3 mb-5 rounded" style={{backgroundColor:"#603c99", width:"280px"}} >
+            <div className="d-flex flex-column align-items-base shadow p-3 mb-5 rounded" style={{backgroundColor:"#603c99", width:"310px"}} >
                     <div className='d-flex align-items-center'>
                     <img src={coin.img} width={"32px"} height={"32px"} className='mx-2'></img>
                     <h3 className="text-light">
                         {`${coin.code}`}
                     </h3>
                 </div>
-                <h3 className="text-light px-2">{`USD : ${coin.value}`}</h3>
+                <div className='d-flex align-items-base'>
+                    <h3 className="text-light px-2" >{`$ ${coin.value.toFixed(2)}`}</h3>
+                    <h3 className=' px-2' style={coin.variant<0 ?{color:"#E72929"}:{color:"green"}}>{`${coin.variant.toFixed(1)}%`}</h3>
+                </div>
+
             </div>
         </Link>
     )

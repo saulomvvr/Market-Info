2be032237657fc554bb66e79f3aec90d070f341b
saulomv <54077670+saulomvvr@users.noreply.github.com>
2024-04-20 23:02:50 -0300

    finished sites links in Information.js

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -23,6 +23,10 @@ const Home = ({ receiveCoin,order }) => {
             popular:1,
             variant:2.5,
             color: '#ffd700',
+            site:   'https://bitcoin.org/en/',
+            whitepaper: 'https://bitcoin.org/bitcoin.pdf',
+            github: 'https://github.com/bitcoin/bitcoin',
+            reddit: 'https://reddit.com/r/bitcoin',
             about: `Bitcoin é uma criptomoeda descentralizada originalmente descrita em 2008 em seu whitepaper por uma pessoa, ou grupo de pessoas, usando o pseudônimo Satoshi Nakamoto. Foi lançado um pouco depois, em janeiro de 2009.
             O Bitcoin é uma moeda digital ponto a ponto, o que significa que toda transação ocorre diretamente entre as partes iguais e independentes, sem a necessidade de nenhum intermediário para permitir ou facilitar. O Bitcoin foi criado, de acordo com as próprias palavras do Nakamoto, para permitir que “pagamentos online sejam enviados diretamente de uma parte para a outra, sem passar por uma instituição financeira”.
             Alguns conceitos para um tipo semelhante de moeda eletrônica descentralizada precedem o BTC, mas o Bitcoin possui o diferencial de ser a primeira criptomoeda a realmente entrar em uso. `
@@ -35,6 +39,10 @@ const Home = ({ receiveCoin,order }) => {
             popular:2,
             variant:-23.7,
             color: '#7a7a7a',
+            site: "https://ethereum.org/pt/",
+            whitepaper: "https://github.com/ethereum/wiki/wiki/White-Paper",
+            github: "https://github.com/ethereum/go-ethereum",
+            reddit: "https://reddit.com/r/ethereum",
             about: `Ethereum é um sistema de blockchain descentralizado e open-source, que possui sua própria criptomoeda, o Ether. O ETH funciona como uma plataforma para várias outras criptomoedas, bem como para a execução de [smart contracts] descentralizados (https://coinmarketcap.com/alexandria/glossary/smart-contract).
             Ethereum foi definido pela primeira vez em um whitepaper de 2013 por Vitalik Buterin. Buterin, junto com outros cofundadores. O financiamento para o projeto foi garantido por uma venda pública online no verão de 2014, sendo lançado oficialmente o blockchain em 30 de julho de 2015.
             O próprio objetivo do Ethereum é se tornar uma plataforma global para aplicativos descentralizados, permitindo que usuários de todo o mundo escrevam e executem softwares que sejam resistente à censura, tempo de inatividade e fraude.`
@@ -47,6 +55,10 @@ const Home = ({ receiveCoin,order }) => {
             popular:3,
             variant:4.7,
             color: '#64cc9a',
+            site: "https://solana.com/",
+            whitepaper: "https://solana.com/solana-whitepaper.pdf",
+            github: "https://github.com/solana-labs",
+            reddit: "https://reddit.com/r/solana",
             about: `Solana é um projeto de código aberto altamente funcional que se baseia na natureza da tecnologia sem permissão do blockchain para oferecer soluções de finanças descentralizadas (DeFi). Embora a ideia e o trabalho inicial do projeto começaram em 2017, a Solana foi oficialmente lançada em março de 2020, pela Solana Foundation com sede em Genebra, Suíça.    
             O protocolo da Solana é projetado para facilitar a criação de aplicativos descentralizados (DApp). Ele visa melhorar a escalabilidade ao introduzir o consenso proof-of-history (PoH), combinado com o consenso subjacente do proof-of-stake (PoS) do blockchain.
             Devido ao modelo de consenso híbrido inovador, Solana aproveita o interesse tanto de traders pequenos, como institucionais. Um foco importante para a Solana Foundation é tornar as finanças descentralizadas acessíveis em uma escala maior.`
@@ -59,6 +71,10 @@ const Home = ({ receiveCoin,order }) => {
             popular:4,
             variant:3.6,
             color: '#ff8c00',
+            site: "https://shibatoken.com/",
+            whitepaper: "https://github.com/shytoshikusama/woofwoofpaper/raw/main/SHIBA_INU_WOOF_WOOF.pdf",
+            github: "",
+            reddit: "https://reddit.com/r/SHIBArmy",
             about: `A moeda Shiba Inu foi criada anonimamente em agosto de 2020 sob o pseudônimo de "Ryoshi". A moeda meme rapidamente ganhou velocidade e valor, à medida que a comunidade de investidores foi atraída pelo charme fofo da moeda juntamente com as manchetes e Tweets de personalidades como Elon Musk e Vitalik Buterin.
             A Shiba Inu buscou ser a contraparte, baseada no Ethereum, da Dogecoin, baseada no algoritmo de mineração Srypt. A Shiba Inu e o token SHIB são partes de uma enxame de criptomoedas com tema de cachorro que incluem a Baby Dogecoin (BabyDoge), Dogecoin (DOGE), JINDO INU (JIND), Alaska Inu (LAS) e Alaskan Malamute Token (LASM). Esses tokens de pouco valor atraíram investidores que perderam a alta do Dogecoin de US$0,0002 para quase US$0,75.
             Era uma vez um cão muito especial. Este cão era o shiba inu, e este cão inspirou milhões de pessoas no mundo todo a investirem seu dinheiro em tokens com a imagem de um cão.
@@ -72,6 +88,10 @@ const Home = ({ receiveCoin,order }) => {
             popular:5,
             variant:4.5,
             color: '#f0e581',
+            site: "http://dogecoin.com/",
+            whitepaper: "https://github.com/dogecoin/dogecoin/blob/master/README.md",
+            github: "https://github.com/dogecoin/dogecoin",
+            reddit: "https://reddit.com/r/dogecoin",
             about: `Dogecoin (DOGE) é baseada no meme popular da internet o "doge" e possui como logotipo um Shiba Inu. A moeda digital de open-source foi criada por Billy Markus de Portland, Oregon e Jackson Palmer de Sydney, Austrália, sendo um fork do Litecoin ocorrido em dezembro de 2013. Os criadores do Dogecoin o imaginaram como algo divertido, uma criptomoeda descontraída que teria maior apelo, indo além do público alvo do Bitcoin, já que foi baseada em meme de cachorro. O CEO da Tesla Elon Musk postou alguns tweets na mídia social dizendo que Dogecoin seria sua moeda favorita.`
         },
         {
@@ -82,6 +102,10 @@ const Home = ({ receiveCoin,order }) => {
             popular:6,
             variant:2.5,
             color: '#3aab94',
+            site: "https://tether.to/",
+            whitepaper: "https://tether.to/wp-content/uploads/2016/06/TetherWhitePaper.pdf",
+            github: "",
+            reddit: "",
             about: `Tether (USDT) é uma moeda digital criada para espelhar o valor do dólar americano. Lançado em 2014, a ideia por trás do Tether era criar uma criptomoeda estável que pudesse ser usada como um dólar digital, ou “stablecoin.” Tethers é ancorada, ou “tethered,” ao preço do dólar americano.
             Inicialmente o Tether usava a rede Bitcoin, Omni Layer, como seu protocolo de transporte. O Tether agora está disponível como token ERC20 no Ethereum. No total, o Tether é emitido nos blockchains do Bitcoin (tanto protocolo Omni e Liquid), Ethereum, EOS e Tron.
             Os tokens Tether são emitidos pela Tether Limited, que possui o mesmo CEO que a corretora de criptomoedas Bitfinex. O Tether alegou anteriormente que a sua moeda seria 100% resguardada pela sua própria reserva, mas seus advogados, em 2019, notaram que a reserva por trás do Tether era de apenas 74%, ou uma reserva fracionária. O Tether percebeu que a definição de “resguardada totalmente” inclui os empréstimos para empresas afiliadas.`
@@ -94,6 +118,10 @@ const Home = ({ receiveCoin,order }) => {
             popular:7,
             variant:-2.5,
             color: '#fffbb8',
+            site: "https://bnbchain.org/en",
+            whitepaper: "",
+            github: "https://github.com/bnb-chain",
+            reddit: "https://reddit.com/r/bnbchainofficial",
             about: `O BNB foi lançado através de uma ICO em 2017, onze dias antes da corretora de criptomoedas Binance ser lançada. Originalmente, ele foi lançado como um token ERC-20 sendo executado na rede Ethereum, com um limite previsto de 200 milhões de moedas, sendo 100 milhões de BNBs oferecidos no ICO. No entanto, as moedas BNB ERC-20 foram trocadas pelo BEP2 BNB, numa corretagem de 1 para 1, em abril de 2019, com o lançamento da mainnet da Binance Chain e, atualmente, não se encontra mais hospedada no Ethereum. O BNB pode ser usado como um método de pagamento, um token de utilidade para pagar por taxas na corretora Binance e para participar nas vendas de tokens nos lançamentos da Binance. O BNB também alimenta a Binance DEX (corretora descentralizada).`
         },
         {
@@ -104,6 +132,10 @@ const Home = ({ receiveCoin,order }) => {
             popular:8,
             variant:-22.5,
             color: '#7ea8fc',
+            site: "https://www.centre.io/usdc",
+            whitepaper: "https://f.hubspotusercontent30.net/hubfs/9304636/PDF/centre-whitepaper.pdf",
+            github: "https://github.com/centrehq/centre-tokens",
+            reddit: "",
             about: `O USD Coin (conhecido pelo seu ticker USDC) é uma stablecoin atrelada ao dólar americano na cotação de 1:1. Cada unidade desta criptomoeda em circulação é lastreada a US$1, mantida numa reserva, numa mistura de dinheiro e títulos do Tesouro Americano de curto prazo. The Centre consortium, responsável por este ativo, alega que o USDC é emitido por instituições financeiras reguladas.
             A stablecoin foi lançada originalmente em uma base limitada, em setembro de 2018. De forma simples, o mantra do USD Coin é “dinheiro digital para uma era digital” — e a stablecoin foi projetada para um mundo em que as transações sem a utilização de dinheiro em espécie estão se tornando mais comuns.    
             O USD Coin demonstrou diversos casos de uso. Além de oferecer um refúgio seguro para os comerciantes de criptomoedas em momentos de alta volatilidade, os responsáveis pela stablecoin defendem que ela também permite que empresas aceitem pagamentos de ativos digitais e movimentem uma série de setores, incluindo finanças descentralizadas e jogos.
@@ -117,6 +149,10 @@ const Home = ({ receiveCoin,order }) => {
             popular:9,
             variant:23.5,
             color: '#FFFFFF',
+            site: "https://xrpl.org/",
+            whitepaper: "https://ripple.com/files/ripple_consensus_whitepaper.pdf",
+            github: "https://github.com/ripple/rippled",
+            reddit: "https://reddit.com/r/ripple",
             about: `XRP é a criptomoeda nativa da Ripple, um sistema de pagamento em criptomoedas criado pela Ripple Labs Inc. XRP é seu “ativo digital construído para pagamentos globais,“ o que implica que a Ripple planeja rivalizar com as transferências de dinheiro geralmente conduzidas pelo sistema bancário. XRP permite que os usuários enviem dinheiro a um custo muito baixo, buscando atrair o interesse de clientes de varejo e bancos. Uma proposta de valor importante da Ripple são seus minúsculos custos de transação, enquanto oferece uma finalidade de transação de menos de cinco segundos.
             A empresa foi fundada em 2012 por Chris Larsen e Jed McCaleb, e é baseada no trabalho de Ryan Fugger, que criou a XRP Ledger em 2012. O XRP Ledger é um livro razão criptografado de código aberto fomentado por uma rede ponto a ponto de nós. McCaleb acabou deixando a Ripple e fundou a Stellar, outra criptomoeda para pagamentos. 
             Após a empresa mudar seu nome original de OpenCoin para Ripple, ela buscou parcerias de alto nível com Bank of America, Santander e Standard Chartered. No entanto, a Ripple logo teve problemas legais com as autoridades reguladoras dos EUA por não cumprir as Leis de Sigilo Bancário, e mais tarde foi rotulada como um título não registrado. Até o começo de 2022, esse processo ainda não foi resolvido. Além disso, a empresa foi acusada de propaganda enganosa no começo da sua existência.`
@@ -129,6 +165,10 @@ const Home = ({ receiveCoin,order }) => {
             popular:10,
             variant:42.5,
             color: '#173a80',
+            site: "https://litecoin.org/",
+            whitepaper: "",
+            github: "https://github.com/litecoin-project/litecoin",
+            reddit: "https://reddit.com/r/litecoin",
             about: `Litecoin (LTC) é uma criptomoeda que foi projetada para fornecer pagamentos rápidos, seguros e com baixo custo, aproveitando as propriedades únicas da tecnologia blockchain.
             A criptomoeda foi criada com base no protocolo Bitcoin (BTC), mas se difere em termos do algoritmo de hashing utilizado, limite máximo, tempo de transação do bloco e alguns outros fatores. A Litecoin possui um tempo de bloco de apenas 2,5 minutos e taxas de transação extremamente baixas, tornando-a adequada para micro-transações e pagamentos em pontos de venda.    
             A Litecoin foi lançada via um cliente open-source na GitHub em 7 de outubro de 2011, e a Rede Litecoin passou a funcionar cinco dias mais tarde, em 13 de outubro de 2011. Desde então, ela explodiu tanto em utilidade, quanto em aceitação pelos comerciantes, e passou grande parte da sua existência entre as dez principais criptomoedas por capitalização de mercado.    
--- src/Components/Information/index.js
+++ src/Components/Information/index.js
@@ -1,22 +1,48 @@
 const Information = ({ coin, themeNowHF, themeNow }) => {
     return (
-        
-        <div /*Fundo da pagina*/className="w-auto mw-100 h-100 mh-100 d-flex justify-content-center p-5" style={{backgroundColor: themeNow}}>
-            <div /*Fundo da informaçao*/className="rounded-5 d-flex bd-highlight" style={{backgroundColor: themeNowHF, width: '95%', height: '95%', maxWidth: '95%'}}>
-                <div /*Info Left*/className="mb-3 flex-fill bd-highlight d-flex flex-column align-items-start" style={{width: '50%'}}>
-                    <div /*Coin Info*/className="d-flex align-items-center">
-                        <img /*Coin Image*/className="mt-3 ms-3" src={coin.img} width={'64px'} height={'64px'}></img>
-                        <h1 /*Coin Name*/className="mt-3 ms-3" style={{fontSize: '64px', color: themeNow}}>{coin.name}</h1>
-                        <h2 /*Coin Name*/className="mt-3 ms-3" style={{fontSize: '64px', color: coin.color}}>{coin.code}</h2>
+
+        <div /*Fundo da pagina*/ className="w-auto mw-100 h-100 mh-100 d-flex justify-content-center p-5" style={{ backgroundColor: themeNow }}>
+            <div /*Fundo da informaçao*/ className="rounded-5 d-flex bd-highlight" style={{ backgroundColor: themeNowHF, width: '95%', height: '95%', maxWidth: '95%' }}>
+                <div /*Info Left*/ className="mb-3 flex-fill bd-highlight d-flex flex-column align-items-start" style={{ width: '50%' }}>
+                    <div /*Coin Info*/ className="d-flex align-items-center">
+                        <img /*Coin Image*/ className="mt-3 ms-3" src={coin.img} width={'64px'} height={'64px'}></img>
+                        <h1 /*Coin Name*/ className="mt-3 ms-3" style={{ fontSize: '64px', color: themeNow }}>{coin.name}</h1>
+                        <h2 /*Coin Name*/ className="mt-3 ms-3" style={{ fontSize: '64px', color: coin.color }}>{coin.code}</h2>
                     </div>
-                    <div /*Coin Price*/className="d-flex align-items-center">
-                        <h2 /*Coin Price USD*/className="mt-3 ms-3" style={{fontSize: '64px', color: themeNow}}>{`${coin.value.toFixed(2)}`}</h2>
-                        <h2 /*Coin Price USD*/className="mt-3 ms-3" style={{fontSize: '32px', color: themeNow}}>{`USD`}</h2>
+                    <div /*Coin Price*/ className="d-flex align-items-center">
+                        <h2 /*Coin Price USD*/ className="mt-3 ms-3" style={{ fontSize: '64px', color: themeNow }}>{`${coin.value.toFixed(2)}`}</h2>
+                        <h2 /*Coin Price USD*/ className="mt-3 ms-3" style={{ fontSize: '32px', color: themeNow }}>{`USD`}</h2>
+                    </div>
+                    <div /*Oficials Medias*/ className="d-flex flex-column bd-highlight mb-3">
+                        <h4 /*Oficials Links*/ className="mt-3 ms-3" style={{ fontSize: '32px', color: themeNow }}>{`Links Oficiais`}</h4>
+                        <div /*Links*/ className="d-flex align-items-center">
+                            {coin.site &&
+                                <h4 /*ST*/ className="mt-3 ms-3 rounded-2 p-1" style={{ backgroundColor: themeNow, fontSize: '16px', color: themeNow }}>
+                                    <a href={coin.site} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: themeNowHF }}>🌐 Site</a>
+                                </h4>
+                            }
+                            {coin.whitepaper &&
+                                <h4 /*WP*/ className="mt-3 ms-3 rounded-2 p-1" style={{ backgroundColor: themeNow, fontSize: '16px', color: themeNow }}>
+                                    <a href={coin.whitepaper} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: themeNowHF }}>📄 Whitepaper</a>
+                                </h4>
+                            }
+                            {coin.github &&
+                                <h4 /*GH*/ className="mt-3 ms-3 rounded-2 p-1" style={{ backgroundColor: themeNow, fontSize: '16px', color: themeNow }}>
+                                    <a href={coin.github} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: themeNowHF }}>🐱 GitHub</a>
+                                </h4>
+                            }
+                            {coin.reddit &&
+                                <h4 /*RD*/ className="mt-3 ms-3 rounded-2 p-1" style={{ backgroundColor: themeNow, fontSize: '16px', color: themeNow }}>
+                                    <a href={coin.reddit} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: themeNowHF }}>🤖 Reddit</a>
+                                </h4>
+                            }
+                        </div>
+
                     </div>
                 </div>
-                <div /*Info Right*/className="mb-3 flex-fill bd-highlight d-flex flex-column-reverse" style={{width: '50%'}}>
-                    <h3 /*Text About Coin*/className="mt-3 ms-3 me-5" style={{fontSize: '16px', color: themeNow}}>{`${coin.about}`}</h3>
-                    <h3 /*About Coin*/className="mt-3 ms-3" style={{fontSize: '32px', color: themeNow}}>{`Sobre ${coin.name}`}</h3>
+                <div /*Info Right*/ className="mb-3 flex-fill bd-highlight d-flex flex-column-reverse" style={{ width: '50%' }}>
+                    <h3 /*Text About Coin*/ className="mt-3 ms-3 me-5" style={{ fontSize: '16px', color: themeNow }}>{`${coin.about}`}</h3>
+                    <h3 /*About Coin*/ className="mt-3 ms-3" style={{ fontSize: '32px', color: themeNow }}>{`Sobre ${coin.name}`}</h3>
                 </div>
             </div>
         </div>
